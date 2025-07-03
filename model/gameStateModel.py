14bd1f89f8acf77a042fdf4f4e85ff439df44198
Rafael Fischer Reichert <50602114+RafaFischerReichert@users.noreply.github.com>
2025-07-03 01:52:58 -0300

    Refactor GameState class: update type hints for possible suspects, weapons, rooms, and players to use concrete Card and Player types

--- model/gameStateModel.py
+++ model/gameStateModel.py
@@ -1,28 +1,30 @@
 from typing import List, Set
+from model.cardModel import Card
+from model.playerModel import Player
 
 class GameState:
     accessible_rooms: List[str]
-    possible_suspects: Set["Card"]
-    possible_weapons: Set["Card"]
-    possible_rooms: Set["Card"]
-    players: List["Player"]
+    possible_suspects: Set[Card]
+    possible_weapons: Set[Card]
+    possible_rooms: Set[Card]
+    players: List[Player]
     user_index: int
 
-    def __init__(self, rooms: List[str], players: List["Player"], user_name: str) -> None:
+    def __init__(self, rooms: List[str], players: List[Player], user_name: str) -> None:
         self.accessible_rooms: List[str] = rooms
-        self.possible_suspects: Set["Card"] = set()
-        self.possible_weapons: Set["Card"] = set()
-        self.possible_rooms: Set["Card"] = set(rooms)
-        self.players: List["Player"] = players
+        self.possible_suspects: Set[Card] = set()
+        self.possible_weapons: Set[Card] = set()
+        self.possible_rooms: Set[Card] = set(rooms)
+        self.players: List[Player] = players
         self.user_index: int = players.index(user_name)
 
-    def set_possible_suspects(self, suspects: List["Card"]) -> None:
+    def set_possible_suspects(self, suspects: List[Card]) -> None:
         self.possible_suspects = set(suspects)
 
-    def set_possible_weapons(self, weapons: List["Card"]) -> None:
+    def set_possible_weapons(self, weapons: List[Card]) -> None:
         self.possible_weapons = set(weapons)
 
-    def set_possible_rooms(self, rooms: List["Card"]) -> None:
+    def set_possible_rooms(self, rooms: List[Card]) -> None:
         self.possible_rooms = set(rooms)
 
     def get_possible_solutions(self) -> List[tuple]:
